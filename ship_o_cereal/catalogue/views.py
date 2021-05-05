9e8b44bbf4bb03b0fac44d17854a7c71f9a75cee
Pallrp <prp@palli.org>
2021-05-05 11:39:55 +0000

    merge funsies

--- ship_o_cereal/catalogue/views.py
+++ ship_o_cereal/catalogue/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.models import *
 
 # Create your views here.
+
 def index(request):
-    return render(request, 'catalogue/index.html')
+    context = {'items' : Items.objects.all().order_by('Name')}
+    return render(request, 'catalogue/index.html', context)
+
+def all_items(request):
+    context = {'items' : Items.objects.all().order_by('Name')}
+    return render(request, 'catalogue/index.html', context)
