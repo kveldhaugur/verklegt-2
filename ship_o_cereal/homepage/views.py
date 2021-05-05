71263cfc3a7b5781a345c19ee6f9b84ed34b15c1
ulfars20 <ulfars20@ru.is>
2021-05-05 09:49:38 +0000

    idk

--- ship_o_cereal/homepage/views.py
+++ ship_o_cereal/homepage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+cereals = [
+    {'name': 'cheerios', 'price': 349},
+    {'name': 'lucky charms', 'price': 345}
+]
+
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/index.html')
+    return render(request, 'homepage/index.html', context={ 'cereals': cereals })
