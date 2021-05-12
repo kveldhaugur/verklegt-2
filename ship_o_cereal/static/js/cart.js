f9da0e543e55d7cd4ff2f2b635862fc14f82026d
Pallrp <prp@palli.org>
2021-05-12 22:56:21 +0000

    pls

--- ship_o_cereal/static/js/cart.js
+++ ship_o_cereal/static/js/cart.js
@@ -4,7 +4,12 @@ for(var i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function() {
         var ItemID = this.dataset.item
         var action = this.dataset.action
-        var quantity = this.dataset.quantity
+        if (this.name === 'intfield') {
+            var quantity = document.getElementById('add-to-cart-amount-btn').value
+        }
+        else {
+            var quantity = this.dataset.quantity
+        }
         console.log('ItemID', ItemID, 'action', action, 'quantity', quantity)
 
         console.log('USER', user)
