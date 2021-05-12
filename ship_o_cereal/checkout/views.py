c31efda0844548fc64cee65ab39c9ed8190e4799
ulfars20 <ulfars20@ru.is>
2021-05-12 15:39:16 +0000

    hiæhjæhjh

--- ship_o_cereal/checkout/views.py
+++ ship_o_cereal/checkout/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from main.models import UserInfo, Country, UserImage
+from main.models import UserInfo, Country, UserImage, ShoppingCart
 
 # Create your views here.
 def index(request):
     context = {
         'data': None
     }
-
     try:
         usrdata = UserInfo.objects.get(AccountConnected=request.user.id)
         info = {
@@ -37,6 +36,22 @@ def index(request):
             'country': ''
         }
     context['data'] = info
-
     context['countries'] = Country.objects.all()
+
+    if request.session.session_key != None:
+        try:
+            cart = ShoppingCart.objects.get(SessionID=request.session.session_key)
+        except ShoppingCart.DoesNotExist:
+            cart = None
+        if cart != None:
+            cart_contains = cart.ItemsInCart.all()
+            items = []
+            for cart_item in cart_contains:
+                item = cart_item.ItemID
+                items.append((item, cart_item.Quantity))
+            context['items_in_cart'] = items
+            return render(request, 'checkout/index.html', context)
+    else:
+        request.session.create()
+    context['items_in_cart'] = None
     return render(request, 'checkout/index.html', context)
